6f999d84bef366bb11ba4f747d56452b9d754faf
quynhlx <m1@MacBook-Air-cua-m1.local>
2024-09-13 20:50:11 +0700

    api login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,10 +19,33 @@ class UserController extends Controller
     {
         $name = $request->input('name');
         $password = $request->input('password');
+        $email = $request->input('email');
 
         return User::insert([
             'name' => $name,
+            'email' => $email,
             'password' => $password,
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        $username = $request->input('username');
+        $password = $request->input('password');
+        // select * from users where name = $username
+        $users = User::query()->where('name', $username)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+        foreach ($users as $user) {
+
+            if ($user->password == $password) {
+                return response()->json(['error' => 'oki'], 200);
+            }
+
+        }
+        return response()->json(['error' => 'error'], 400);
+
     }
 }
