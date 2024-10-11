e2f47f4f37e5756570f89f507de733945f2c9427
Cuong241204 <cuong242924@gmail.com>
2024-10-11 14:17:50 +0000

    code update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,6 +79,7 @@ class UserController extends Controller
     }
     public function updateUser(UpdateUserRequest $request, $id)
     {
+        $id=$request->input('id');
         $users = $this->userService->updateUser($request, $id);
 
         if (!$users) {
